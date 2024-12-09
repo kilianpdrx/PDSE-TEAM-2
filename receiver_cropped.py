af2828dcbfb0aa3b853c6bc8e4c1cf2cb2f6f405
kilianpdrx <kilianpdrx@icloud.com>
2024-12-09 23:37:35 +0100

    Pas mal les flux

--- receiver_cropped.py
+++ receiver_cropped.py
@@ -6,17 +6,51 @@ import numpy as np
 from FE_modif import FeatureExtractor2
 from functions_final import *
 
+
+
+def calibrate(extractor, cropped_person):
+    # Détecter les caractéristiques de la personne
+    target_features = extractor(cropped_person)
+
+
+    return target_features
+
+
 extractor = FeatureExtractor2(
     model_name='osnet_x0_25',
     model_path='osnet_x0_25_imagenet.pth',
     device='cpu'
 )
-    
-# URL du flux vidéo
-url = "http://10.11.6.148:5000/video_feed"  # Remplacez par l'adresse de votre Raspberry Pi
+
+
+target_features = None  # Caractéristiques de la personne calibrée
+list_target_features = []  # Liste des caractéristiques de la personne calibrée
+list_target_features2 = []  # Liste des caractéristiques de la personne calibrée
+
+min_number_features = 30  # Nombre minimal de features pour la calibration
+calibrated = False  # Statut de calibration
+similarity_threshold = 0.75  # Seuil de similarité pour la comparaison de ReID
+
+index_frame = 0 # on est obligés d'en mettre 2 parce qu'on saute des frames
+frame_count = 0
+total_processing_time = 0.0
+start_time = time.time()
+frame_jump = 1  # Ignorer certaines frames pour optimiser
+
+
+
+
+
+
+# URL du flux vidéo, on peut mettre ça dans un navigateur pour voir le flux
+url = {"cropped": "http://10.11.6.148:5000/cropped_feed",
+       "full": "http://10.11.6.148:5000/full_feed"}
+
+CHOIX_URL = "cropped" # or full
+
 
 # Connexion au flux vidéo
-stream = requests.get(url, stream=True)
+stream = requests.get(url[CHOIX_URL], stream=True)
 if stream.status_code != 200:
     print("Impossible de se connecter au flux vidéo.")
     exit()
@@ -25,30 +59,84 @@ if stream.status_code != 200:
 bytes_data = b""
 frame_counter = 0
 
-for chunk in stream.iter_content(chunk_size=1024):
-    bytes_data += chunk
 
-    # Détecter la fin d'une image
-    a = bytes_data.find(b'\xff\xd8')  # Début de l'image JPEG
-    b = bytes_data.find(b'\xff\xd9')  # Fin de l'image JPEG
+if CHOIX_URL == 1:
+    for chunk in stream.iter_content(chunk_size=1024):
+        bytes_data += chunk
 
-    if a != -1 and b != -1:
-        # Extraire l'image JPEG
-        jpeg_data = bytes_data[a:b+2]
-        bytes_data = bytes_data[b+2:]
+        # Détecter la fin d'une image
+        a = bytes_data.find(b'\xff\xd8')  # Début de l'image JPEG
+        b = bytes_data.find(b'\xff\xd9')  # Fin de l'image JPEG
 
-        # Convertir l'image en format numpy pour OpenCV
-        nparr = np.frombuffer(jpeg_data, np.uint8)
-        cropped_person = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        
-        target_features, target_image = calibrate(extractor, cropped_person)
 
-        # Afficher l'image dans une fenêtre distincte
-        cv2.imshow(f"Cropped Person {frame_counter}", cropped_person)
-        frame_counter += 1
+        if a != -1 and b != -1:
+            # Extraire l'image JPEG
+            index_frame += 1
+            
+            
+            jpeg_data = bytes_data[a:b+2]
+            bytes_data = bytes_data[b+2:]
 
-        # Quittez avec la touche 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Convertir l'image en format numpy pour OpenCV
+            nparr = np.frombuffer(jpeg_data, np.uint8)
+            cropped_person = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            
+            
+            if not calibrated:
+                # Étape de calibration
+                target_features = calibrate(extractor, cropped_person)
+                if target_features is not None:
+                    print(f"Calibration : {index_frame/min_number_features * 100:.2f}%")
+                    list_target_features.append(target_features)
+                    
+                else:
+                    print("Veuillez vous placer devant la caméra pour la calibration.")
+                        
+                if len(list_target_features) >= min_number_features:
+                    print("Calibration terminée. Personne cible enregistrée.")
+                    calibrated = True
+                    print("Durée de la calibration : ", time.time() - start_time)
+                    # cv2.imshow("Personne calibrée", target_image)
+                    # cv2.waitKey(0)
+                    # cv2.destroyWindow("Personne calibrée")
+            
+            
+            
+            
+            # if target_features is not None:
+            #     print("Calibration réussie.")
 
-cv2.destroyAllWindows()
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            cv2.imshow("Personne calibrée", cropped_person)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+else:
+    for chunk in stream.iter_content(chunk_size=1024):
+        bytes_data += chunk
+
+        # Détecter la fin d'une image
+        a = bytes_data.find(b'\xff\xd8')  # Début de l'image JPEG
+        b = bytes_data.find(b'\xff\xd9')  # Fin de l'image JPEG
+
+
+        if a != -1 and b != -1:
+            # Extraire l'image JPEG
+            index_frame += 1
+            
+            
+            jpeg_data = bytes_data[a:b+2]
+            bytes_data = bytes_data[b+2:]
+
+            # Convertir l'image en format numpy pour OpenCV
+            nparr = np.frombuffer(jpeg_data, np.uint8)
+            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            cv2.imshow("Full frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+
+cv2.destroyAllWindows() 
+
